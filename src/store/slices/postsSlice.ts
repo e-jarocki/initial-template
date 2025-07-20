c0c84140cdc0079fb975514b2299fe4122bd633a
Eyarotskiy <eyarotskiy94@gmail.com>
2025-07-20 18:10:21 +0200

    feat: small change of structure

--- src/store/slices/postsSlice.ts
+++ src/store/slices/postsSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Post } from '@/entities/Post/model/types';
+import { Post } from '@/entities/Post/model';
 
 type PostsState = Post[];
 
